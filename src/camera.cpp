f5498b8757ef580e2657aa3c172b31eb1bb3a32e
Samuel <samuel.saxgarn@gmail.com>
2026-02-03 12:09:17 +0100

    camera fix

--- src/camera.cpp
+++ src/camera.cpp
@@ -21,17 +21,17 @@ mat4f Camera::WorldToViewMatrix() const noexcept
 	//		inverse(T(p)*R) = inverse(R)*inverse(T(p)) = transpose(R)*T(-p)
 	// Since now there is no rotation, this matrix is simply T(-p)
 
-	mat4f rotation_matrix = mat4f::rotation(0.0f, -yaw, -pitch);  
+	mat4f rotation_matrix = mat4f::rotation(0.0f, yaw, pitch);  
 	mat4f translation_matrix = mat4f::translation(-m_position); // Move the world the opposite way of the camera position
 
-	return rotation_matrix * translation_matrix;
+	return transpose(rotation_matrix) * translation_matrix;
 }
 
 
 void Camera::Rotate(float delta_yaw, float delta_pitch) noexcept
 {
-	yaw += delta_yaw;
-	pitch += delta_pitch;
+	yaw += delta_yaw; //lägg till förändringen i yaw
+	pitch += delta_pitch; //lägg till förändringen i pitch
 
 	// Clamp pitch to avoid gimbal lock
 	const float limit = 1.5f; // ~85 degrees (cause 85 degrees ~ 1.5 radians)
